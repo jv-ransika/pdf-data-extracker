edad71a51a37e450a09fc4d8466423411224285d
jv_ransika <jvgransika@gmail.com>
2024-06-20 09:49:32 +0530

    good speed

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
+import PySimpleGUI as sg
+import os
 from pdf import Pdf
 
-def pdf_to_csv(pdf_path, csv_path):
-    # Open the PDF file
-    with pdfplumber.open(pdf_path) as pdf:
-        print(pdf.pages[0].extract_text())
-        
-        # Create a CSV file to write data
-        # with open(csv_path, 'w', newline='') as csvfile:
-        #     csvwriter = csv.writer(csvfile)
+
+def process_pdf(DIR, pdf_path):
+    p = Pdf(DIR, pdf_path)
+    p_count = p.get_pages_count()
+    pp_count = 0
+
+    window['p_bar_2'].update(current_count=0, max=p_count)
+    window['p_count_l'].update(f'{pp_count} pages processed from {p_count} pages')
+
+    for _ in p.to_csv():
+        pp_count += 10
+        window['p_bar_2'].update(current_count=pp_count)
+        window['p_count_l'].update(f'{pp_count} pages processed from {p_count} pages')
+
+    
+
+# Define the window's contents
+layout = [
+    [sg.Text('Input Foldder', size=(12,1)), sg.Input(key='folder'), sg.FolderBrowse()],
+    [sg.Button('Process')],
+    [sg.HorizontalLine()],
+    [sg.Text('o files processed from 0 files', k='f_count_l')],
+    [sg.ProgressBar(100, size=(55, 20), key='p_bar_1')],
+    [sg.Text('o pages processed from 0 pages', k='p_count_l')],
+    [sg.ProgressBar(100, size=(55, 20), key='p_bar_2')]
+]
+
+# Create the window
+window = sg.Window('PDF Data Extracker', layout, element_justification= 'c')
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED:
+        break
+    
+    # Output a message to the window
+    if event == 'Process':
+        if not os.path.exists(values['folder']):
+            sg.popup_ok("Folder doesn't exists")
+        elif not os.path.isdir(values['folder']):
+            sg.popup_ok('Invalied folder path')
+        else:
+            f_path = values['folder']
+            pdf_files = [f for f in os.listdir(f_path) if f.endswith('.pdf')]
+            is_process = sg.popup_yes_no(f'{len(pdf_files)} pdf files found. Do you want to process ?')
             
-        #     # Iterate over all the pages
-        #     for page in pdf.pages:
-        #         # Extract text from the page
-        #         text = page.extract_text()
-        #         if text:
-        #             # Split text into lines
-        #             lines = text.split('\n')
-        #             for line in lines:
-        #                 # Split lines into words
-        #                 row = line.split()
-        #                 csvwriter.writerow(row)
-
-# Example usage
-# pdf_to_csv('POS Transaction History Ext Jan - Dec 2015.pdf', 'output.csv')
-
-pdf = Pdf('/home/ransika/Documents/Projects/pdf-data-extracker', 'POS Transaction History Ext Jan - Dec 2015.pdf')
-
-pdf.to_csv()
+            if is_process == 'Yes':
+                fp_count=0
+                window['p_bar_1'].update(current_count= 0, max=len(pdf_files))
+                window['f_count_l'].update(f'{fp_count} files processed from {len(pdf_files)} files')
+                
+                for pdf_path in pdf_files:
+                    process_pdf(f_path, pdf_path)
+                    fp_count += 1
+                    window['p_bar_1'].update(current_count= fp_count)
+                    window['f_count_l'].update(f'{fp_count} files processed from {len(pdf_files)} files')
+                                 
+                
+                
+# Finish up by removing from the screen
+window.close()
--- pdf.py
+++ pdf.py
@@ -1,22 +1,23 @@
+
 import os
 import re
-import pdfplumber
+from pypdf import PdfReader
 import csv
 
 
 class Pdf:
     PATTERN = re.compile(
-    r'(?P<DATE>\w{3} \d{2} \d{4}\s+\d{2}:\d{2}(?:AM|PM))\s+'            # Date and Time
-    r'(?P<TRANS>\d+)\s+'                          # Transaction Number
-    r'((?P<DESCRIPTION>.+?))\s+'                        # Description
     r'(?P<QTY>\d+)\s+'                          # Quantity
+    r'((?P<DESCRIPTION>.+?))\s+'                        # Description
     r'(?P<PRICE>-?\d+\.\d{2})\s+'                   # Price
+    r'(?P<TRANS>\d+)\s+'                          # Transaction Number
+    r'(?P<USER>[A-Za-z0-9-]+)\s+'                       # Account User
+    r'(?P<DATE>\w{3} \d{2} \d{4}\s+\d{2}:\d{2}(?:AM|PM))\s+'            # Date and Time
     r'(?P<FEE>-?\d+\.\d{2})\s+'                 # Fee
     r'(?P<DISCOUNT>-?\d+\.\d{2})\s+'                 # Discount
     r'(?P<TAX>-?\d+\.\d{4})\s+'                 # Tax
     r'(?P<INSURANCE>-?\d+\.\d{2})\s+'                 # Insurance
-    r'(?P<EXTPRICE>-?\d+\.\d{2})\s+'                 # Extended Price
-    r'(?P<USER>[A-Za-z0-9-]+)'                       # Account User
+    r'(?P<EXTPRICE>-?\d+\.\d{2})'                 # Extended Price
 )
     EX_DATA_DIR = 'exreacted_data'
 
@@ -34,10 +35,16 @@ class Pdf:
 
     def to_csv(self):
         csv_path = os.path.join(self.d_name, Pdf.EX_DATA_DIR, self.f_name[:-3] + 'csv')
+        
+        if os.path.exists(csv_path):
+            os.remove(csv_path)
+        
         file_exists = os.path.exists(csv_path)
         batch_size = 10  # Number of pages to process before writing to CSV
 
-        with pdfplumber.open(self.full_path) as pdf, open(csv_path, mode='a' if file_exists else 'w', newline='') as csvfile:
+        pdf = PdfReader(self.full_path)
+        
+        with open(csv_path, mode='a' if file_exists else 'w', newline='') as csvfile:
             writer = None
             headers = None
 
@@ -61,11 +68,13 @@ class Pdf:
     def get_data_from_page(self, pdf, page_num):
         page = pdf.pages[page_num]
         text = page.extract_text()
-        return [m.groupdict() for m in Pdf.PATTERN.finditer(text)]
+        data = [m.groupdict() for m in Pdf.PATTERN.finditer(text)]
+        
+        keyorder = ["DATE", "TRANS", "DESCRIPTION", "QTY", "PRICE", "FEE", "DISCOUNT", "TAX", "INSURANCE", "EXTPRICE", "USER"]
+        return [{k:d[k] for k in keyorder if k in d} for d in data]
 
     def is_exist(self) -> bool:
         return os.path.exists(self.full_path)
 
     def get_pages_count(self):
-        with pdfplumber.open(self.full_path) as pdf:
-            return len(pdf.pages)
+        return len(PdfReader(self.full_path).pages)
